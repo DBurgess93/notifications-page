ad16727e1a79b943f3410c56ac91db719f91a4c6
Daniel Burgess <dan-burgess@hotmail.com>
2023-01-21 16:43:54 +1030

    Add JQuery

--- assets/notification.js
+++ assets/notification.js
@@ -7,3 +7,10 @@ function markAllAsRead() {
     elements.item(i).style.background = "white"
   }
 }
+
+$(document).ready(function() {
+  let notif = document.getElementsByClassName
+  $('notif').click(function() {
+      notif.style.background = "green";
+  });
+});
